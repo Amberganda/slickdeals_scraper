aa684498ff29408fc5391dbd6f629cd679a8ddbc
Amber Taranto <ambzzz@gmail.com>
2019-08-11 20:50:55 -0700

    fixed prices to include numeric as well as price description ie. 2 for 0

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,16 +19,31 @@ class Scraper
         self.deals.each do |node|
             title = node.css("a.itemTitle").text
             title = coder.decode(title)
+            if !title
+                next
+            end
             puts title.strip
+
+        
 
             merchant = node.css("a.itemStore").text
             puts merchant
 
-            price = node.css("div.itemPrice").text.strip#.split(" ").first
-            puts price
+            raw_price = node.css("div.itemPrice").text.strip
+            matches = raw_price.match(/\$(\d*\.?\d{0,2})/)
+            if matches != nil
+                price = matches[1].to_f
+                puts "price #{price}" 
+            end
+
+            matches = raw_price.match(/(.*\$\d*\.?\d{0,2})/)
+            if matches != nil
+                price_description = matches[1]
+                puts "price description #{price_description}"
+            end
 
             rating = node.css("span.likesLabel").text.strip
-            puts rating
+            puts "rating #{rating}"
 
             puts " "
 
@@ -41,3 +56,6 @@ class Scraper
 
   
 end
+
+#.*\$\d*\.?\d{0,2} this will match a string description
+#\$\d*\.?\d{0,2} matches $xx.xx numeric
