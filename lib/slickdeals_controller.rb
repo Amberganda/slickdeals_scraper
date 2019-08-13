9a43cddf29d65f6f43e92de281fb853b285566c2
Amber Taranto <ambzzz@gmail.com>
2019-08-12 18:31:20 -0700

    added REPL

--- lib/slickdeals_controller.rb
+++ lib/slickdeals_controller.rb
@@ -5,9 +5,29 @@ class SlickdealsController
         scraper = Scraper.new
         # deals = scraper.deals
         scraper.make_deals
-        Deal.all.each do |deal|
-            puts "#{deal.title} - #{deal.price} - #{deal.price_description}"
+
+        while true
+            input = gets
+
+            case input.strip
+            when "list all"
+                self.list_all
+            when "exit"
+                break
+            end
+
         end
+        # Deal.all.each do |deal|
+        #     puts "#{deal.title} - #{deal.price} - #{deal.price_description}"
+        # end
     end
+
+    def list_all
+        Deal.all.each_with_index do |deal, index|
+            puts "#{index}. #{deal.title} - #{deal.price} - #{deal.price_description}"
+        end
+        
+    end
+
 
 end
